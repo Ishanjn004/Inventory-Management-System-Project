6a6506b65b2f5911a6490e4a75feaff8ca61067a
Ishan Jain <ishanjain2011004@gmail.com>
2025-04-12 19:45:34 +0530

    added new databases of good reciepts

--- create_db.py
+++ create_db.py
@@ -18,6 +18,7 @@ try:
         cursor.execute("CREATE TABLE IF NOT EXISTS supplier(invoice INTEGER PRIMARY KEY AUTO_INCREMENT,name text,contact text,description text)")
         cursor.execute("CREATE TABLE IF NOT EXISTS category(cid INTEGER PRIMARY KEY AUTO_INCREMENT,name text)")
         cursor.execute("CREATE TABLE IF NOT EXISTS product(pid INTEGER PRIMARY KEY AUTO_INCREMENT,Category text, Supplier text,name text,price text,qty text,status text)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS purchase_return(rid INTEGER PRIMARY KEY AUTO_INCREMENT,Category text, Supplier text,name text,qty text,message text)")
         cursor.execute("CREATE TABLE IF NOT EXISTS sales(name text,date DATE,qty_sold text,current_stock text)")
         print("Table created successfully!")
 except Error as e:
