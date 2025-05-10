cfd2de89a4468e833499a90192f517f640fae318
Ishan Jain <125076845+Ishanjn004@users.noreply.github.com>
2025-05-10 11:19:32 +0530

    Update email_pass.py

--- email_pass.py
+++ email_pass.py
@@ -1,2 +1,2 @@
-email_=''
-pass_=''
+email_=''      #write your email which you want from otp to be sent
+pass_=''       #write the key or password generated using google app password under 2-step verification
